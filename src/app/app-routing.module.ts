52b6b5a6f0739bf15e11a40f711636d510af3ae4
Judah Goff <bgoff1@harding.edu>
2019-09-24 14:01:15 -0500

    Add components, work on moving over stuff from other repository
    Signed-off-by: Judah Goff <bgoff1@harding.edu>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BetrayalComponent } from './betrayal/betrayal.component';
+import { HelpComponent } from './help/help.component';
+import { LabComponent } from './lab/lab.component';
+import { LevelingComponent } from './leveling/leveling.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'betrayal', component: BetrayalComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'leveling', component: LevelingComponent },
+  { path: 'lab', component: LabComponent },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BetrayalComponent } from './betrayal/betrayal.component';
+import { HelpComponent } from './help/help.component';
+import { LabComponent } from './lab/lab.component';
+import { HomeComponent } from './home/home.component';
+import { LevelingComponent } from './leveling/leveling.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BetrayalComponent,
+    HelpComponent,
+    LabComponent,
+    HomeComponent,
+    LevelingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
